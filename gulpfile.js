683e88316c4d361ee70bd2254c920fab7df855a8
allythy <allythyrenan@gmail.com>
2018-02-22 00:32:53 -0300

    adicionando o sass e removendo a cor do link do footer

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,14 @@
 var gulp = require( 'gulp' );
-var minifycss = require( 'gulp-minify-css' );
-var concat = require( 'gulp-concat' );
-var rename = require( 'gulp-rename' );
+var sass = require('gulp-sass');
 
-
-gulp.task( 'css', function() {
-  gulp.src( './src/css/*.css' )
-    .pipe(concat('concat.js'))
-    .pipe(rename('main.min.css'))
-    .pipe( minifycss() )
-    .pipe( gulp.dest( './src/css' ) )
+gulp.task('sass', function () {
+    return gulp.src('src/style/sass/*.sass')
+      .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+      .pipe(gulp.dest('./src/style/css'));
 });
 
 gulp.task( 'watch', function() {
-  gulp.watch( './src/css/*.css', [ 'css' ] );
+  gulp.watch( './src/style/sass/*.sass', [ 'sass' ] );
 });
 
-gulp.task('default', ['watch' , 'css']);
+gulp.task('default', ['watch' , 'sass']);
