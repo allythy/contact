2e66f0aa5ce12dc1c15ec3aabbff90f4ba46342d
allythy <allythyrenan@gmail.com>
2018-02-25 18:50:54 -0300

    alterando package.json e modularizando o sass

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,12 @@ var sass = require('gulp-sass');
 
 gulp.task('sass', function () {
     return gulp.src('src/style/sass/*.sass')
-      .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-      .pipe(gulp.dest('./src/style/css'));
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(gulp.dest('./src/style/css'));
 });
 
 gulp.task( 'watch', function() {
-  gulp.watch( './src/style/sass/*.sass', [ 'sass' ] );
+  gulp.watch( './src/style/**/*.sass', [ 'sass' ] );
 });
 
 gulp.task('default', ['watch' , 'sass']);
